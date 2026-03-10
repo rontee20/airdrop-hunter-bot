9aafc2ff681bdcc84c673f72c451905648791969
rontee20 <134508017+rontee20@users.noreply.github.com>
2026-03-11 00:53:18 +0530

    Update telegram.js

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -1,29 +1,16 @@
-const axios = require('axios');
+const axios = require("axios");
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
 async function sendTelegram(message) {
-    const BOT_TOKEN = process.env.BOT_TOKEN;
-    const CHAT_ID = process.env.CHAT_ID;
-    
-    // Safety: Telegram crashes if HTML tags aren't closed or have illegal characters
-    const safeMessage = message
-        .replace(/&(?!(amp|lt|gt|quot|apos);)/g, '&amp;') 
-        .replace(/<(?!\/?(b|i|a|pre|code|u|s|strike|del|strong|em)>)/g, '&lt;')
-        .replace(/>(?<!<(b|i|a|pre|code|u|s|strike|del|strong|em)\b[^>]*) /g, '&gt;');
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
-    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
-
-    try {
-        await axios.post(url, {
-            chat_id: CHAT_ID,
-            text: safeMessage,
-            parse_mode: 'HTML',
-            disable_notification: false, // 🔔 This forces the notification sound
-            disable_web_page_preview: false
-        });
-        console.log("✅ Alpha sent to Telegram!");
-    } catch (error) {
-        console.error("❌ Telegram Error:", error.response ? error.response.data : error.message);
-    }
+  await axios.post(url, {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "Markdown"
+  });
 }
 
-module.exports = sendTelegram;
+module.exports = { sendTelegram };
