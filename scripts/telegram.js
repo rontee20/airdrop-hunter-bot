67f6ac86cc2d584ee871d767ebc49604542992b5
rontee20 <134508017+rontee20@users.noreply.github.com>
2026-03-11 01:01:15 +0530

    Update telegram.js

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -1,16 +1,13 @@
-const axios = require("axios");
-
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const CHAT_ID = process.env.CHAT_ID;
-
 async function sendTelegram(message) {
-  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
-  await axios.post(url, {
-    chat_id: CHAT_ID,
-    text: message,
-    parse_mode: "Markdown"
-  });
+    console.log("Sending message:", message);
+
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+    const res = await axios.post(url, {
+        chat_id: CHAT_ID,
+        text: message
+    });
+
+    console.log("Telegram response:", res.data);
 }
-
-module.exports = { sendTelegram };
