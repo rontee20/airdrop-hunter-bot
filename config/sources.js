5273505f3335fd1f1c3f914b89fda83b9697a8bc
rontee20 <134508017+rontee20@users.noreply.github.com>
2026-03-11 10:12:15 +0530

    Create sources.js

---
+++ config/sources.js
@@ -0,0 +1,16 @@
+export const telegramChannels = [
+  "https://t.me/s/airdrops_io",
+  "https://t.me/s/CryptoRank_io",
+  "https://t.me/s/ICOAnalytics"
+];
+
+export const twitterAccounts = [
+  "DefiIgnas",
+  "TheDeFinvestor",
+  "CryptoRank_io"
+];
+
+export const websites = [
+  "https://airdrops.io/feed/",
+  "https://cointelegraph.com/rss"
+];
