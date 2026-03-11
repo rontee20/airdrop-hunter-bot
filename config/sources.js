e7c895e829e518efcd0421fc87a11430093cf955
rontee20 <134508017+rontee20@users.noreply.github.com>
2026-03-11 10:21:38 +0530

    Update sources.js

--- config/sources.js
+++ config/sources.js
@@ -1,7 +1,11 @@
 export const telegramChannels = [
-  "https://t.me/s/airdrops_io",
-  "https://t.me/s/CryptoRank_io",
-  "https://t.me/s/ICOAnalytics"
+
+"https://t.me/s/airdrops_io",
+"https://t.me/s/CryptoRank_io",
+"https://t.me/s/ICOAnalytics",
+"https://t.me/s/CryptoDiffer",
+"https://t.me/s/DeFi_Alpha"
+
 ];
 
 export const twitterAccounts = [
